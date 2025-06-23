155995928f4a5fdf6357e34be645268d88e07578
lsadsad <levin@okdomo.com>
2025-06-23 15:25:02 -0700

    Layer Name Emoji
    Gemini - added emoji

--- code.js
+++ code.js
@@ -1,33 +1,28 @@
 "use strict";
-// This plugin will open a window to prompt the user to enter a number, and
-// it will then create that many rectangles on the screen.
-// This file holds the main code for plugins. Code in this file has access to
-// the *figma document* via the figma global object.
-// You can access browser APIs in the <script> tag inside "ui.html" which has a
-// full browser environment (See https://www.figma.com/plugin-docs/how-plugins-run).
-// This shows the HTML page in "ui.html".
-figma.showUI(__html__);
-// Calls to "parent.postMessage" from within the HTML page will trigger this
-// callback. The callback will be passed the "pluginMessage" property of the
-// posted message.
+// This plugin allows the user to add a colored emoji to the beginning of a layer's name.
+// This file holds the main code for the plugin. It has access to the figma document.
+// You can access browser APIs in the <script> tag inside "ui.html".
+// Show the HTML page in "ui.html".
+figma.showUI(__html__, { width: 240, height: 180 });
+// Handle messages from the HTML page.
 figma.ui.onmessage = (msg) => {
-    // One way of distinguishing between different types of messages sent from
-    // your HTML page is to use an object with a "type" property like this.
-    if (msg.type === 'create-shapes') {
-        // This plugin creates rectangles on the screen.
-        const numberOfRectangles = msg.count;
-        const nodes = [];
-        for (let i = 0; i < numberOfRectangles; i++) {
-            const rect = figma.createRectangle();
-            rect.x = i * 150;
-            rect.fills = [{ type: 'SOLID', color: { r: 1, g: 0.5, b: 0 } }];
-            figma.currentPage.appendChild(rect);
-            nodes.push(rect);
+    if (msg.type === 'add-emoji') {
+        const selectedLayers = figma.currentPage.selection;
+        if (selectedLayers.length === 0) {
+            figma.notify('Please select at least one layer.');
         }
-        figma.currentPage.selection = nodes;
-        figma.viewport.scrollAndZoomIntoView(nodes);
+        else {
+            for (const layer of selectedLayers) {
+                // Prepend the emoji to the layer name.
+                layer.name = msg.emoji + ' ' + layer.name;
+            }
+        }
     }
-    // Make sure to close the plugin when you're done. Otherwise the plugin will
-    // keep running, which shows the cancel button at the bottom of the screen.
-    figma.closePlugin();
+    if (msg.type === 'cancel') {
+        figma.closePlugin();
+    }
+    // Close the plugin after adding the emoji.
+    if (msg.type === 'add-emoji') {
+        figma.closePlugin();
+    }
 };
