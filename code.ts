cb9ae75f39f313d852bbfc092ffdc474ec5fbea5
lsadsad <levin@okdomo.com>
2025-06-23 23:33:24 -0700

    Add emoji replace and clear functionality to plugin
    Enhanced the Figma plugin to allow users to replace an existing emoji or clear it from the beginning of a layer's name, in addition to adding one. Updated the UI to include a 'Clear Emoji' button and improved messaging.
    
    Synchronized the emoji list between code and UI, and set an initial window size for the plugin. Updated TypeScript config to use 'es2019' lib.

--- code.ts
+++ code.ts
@@ -1,26 +1,66 @@
-// This plugin allows the user to add a colored emoji to the beginning of a layer's name.
+// This plugin allows the user to add, replace, or clear a colored emoji at the beginning of a layer's name.
 
-// Show the HTML page in "ui.html". This now allows the window to be resized.
-figma.showUI(__html__);
+// This list should be kept in sync with the emojis in ui.html
+const EMOJI_LIST = ['🔴', '🟠', '🟡', '🟢', '🔵', '🟣', '⚫️', '⚪️'];
+
+// Show the HTML page in "ui.html", setting an initial size.
+// The Figma plugin window is resizable by the user from this initial size.
+figma.showUI(__html__, { width: 240, height: 400 });
 
 // Handle messages from the HTML page.
-figma.ui.onmessage = (msg: { type: string, emoji: string }) => {
-  if (msg.type === 'add-emoji') {
-    const selectedLayers = figma.currentPage.selection;
+figma.ui.onmessage = (msg: { type: string, emoji?: string }) => {
+  const selectedLayers = figma.currentPage.selection;
 
+  // Handle adding/replacing an emoji
+  if (msg.type === 'add-emoji') {
     if (selectedLayers.length === 0) {
       figma.notify('Please select at least one layer.');
-    } else {
-      for (const layer of selectedLayers) {
-        // Prepend the emoji to the layer name.
-        layer.name = msg.emoji + ' ' + layer.name;
+      return;
+    }
+    for (const layer of selectedLayers) {
+      const currentName = layer.name;
+      let oldEmojiFound = false;
+      for (const oldEmoji of EMOJI_LIST) {
+        if (currentName.startsWith(oldEmoji)) {
+          const restOfName = currentName.substring(oldEmoji.length).trimStart();
+          layer.name = msg.emoji + ' ' + restOfName;
+          oldEmojiFound = true;
+          break;
+        }
       }
-      // Notify the user that the emoji was added.
-      figma.notify('Emoji added!');
+      if (!oldEmojiFound) {
+        layer.name = msg.emoji + ' ' + currentName;
+      }
+    }
+    figma.notify('Emoji updated!');
+  }
+
+  // Handle clearing an emoji
+  else if (msg.type === 'clear-emoji') {
+    let anEmojiWasCleared = false;
+    if (selectedLayers.length === 0) {
+      figma.notify('Please select at least one layer.');
+      return;
+    }
+    for (const layer of selectedLayers) {
+      const currentName = layer.name;
+      for (const emoji of EMOJI_LIST) {
+        if (currentName.startsWith(emoji)) {
+          layer.name = currentName.substring(emoji.length).trimStart();
+          anEmojiWasCleared = true;
+          break;
+        }
+      }
+    }
+    if (anEmojiWasCleared) {
+      figma.notify('Emoji cleared!');
+    } else {
+      figma.notify('No matching emoji to clear.');
     }
   }
 
-  if (msg.type === 'cancel') {
+  // Handle closing the plugin
+  else if (msg.type === 'cancel') {
     figma.closePlugin();
   }
 };
